355dacb8b0ae50b0d477d4d502e2d4f014878388
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-11-01 15:25:09 -0500

    mapapiupdate

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Registration from'./Registration/index.js';
+import Map from './Map/MyGoogleMap.js';
+import './App.css';
 import Cookies from 'js-cookie';
 import Login from './Login/index.js';
 import Menu from './Menu/index.js';
+
 // import your components
 // load each one individually in the App component below
 // and test each one separately
 
+
+ // our location object from earlier
+
 function App() {
   return (
     <div className="App">
-    <Menu/>
+      <Map/>
+
     </div>
   );
 }
